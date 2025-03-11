aa4128949cc045e2059f0fd1af6e410567e02b50
iLinn <11552398+iLinn@users.noreply.github.com>
2025-03-11 18:29:41 +0200

    Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-learn-tenzies-game
 Learn React: Capstone Project 1: Tenzies Game
+
+
+## Building App
+
+```js
+npm i && npm run dev
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,11 @@
+import './Main.css';
+
+function MainContent() {
+  return (
+    <main>
+      <h1>Hi there</h1>
+    </main>
+  );
+}
+
+export default MainContent;
